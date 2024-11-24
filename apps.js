fcce89bee30eed289aeba8836fcde6d65e836a01
Sabbir Hosen <rsabbirhossain.official@gmail.com>
2024-11-25 03:44:57 +0600

    animation

---
+++ apps.js
@@ -0,0 +1,12 @@
+var buton=document.getElementById("buton")
+var body1=document.getElementById("body1")
+
+function buton1(){
+body1.style.backgroundColor="white"
+h11.style.color="black"
+buton.style.borderRadius="40px"
+buton.style.transform="scale(0.9)"
+setTimeout(function(){
+buton.style.transform="scale(1)"
+},100)
+}
